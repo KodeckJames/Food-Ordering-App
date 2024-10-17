f9f8a98bc92c90a39eee5fbff050a602f07ed6fd
Kodeck James <kodeckjames095@gmail.com>
2024-10-17 17:04:56 +0300

    FlatList implemented

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
-import {  View, StatusBar, ScrollView } from 'react-native';
+import {  View, StatusBar, ScrollView, FlatList } from 'react-native';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
 
 export default function MenuScreen() {
   return (
-    <View className=' bg-white flex-1' style={{ paddingTop: StatusBar.currentHeight }}>
+    <View className=' bg-gray-300 flex-1' style={{ paddingTop: StatusBar.currentHeight }}>
       <StatusBar barStyle={'light-content'} backgroundColor={'purple'}/>
-      <ScrollView>
-      <ProductListItem product={products[5]}/>
-        <ProductListItem product={products[1]} />
-        </ScrollView>
+      <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} numColumns={2} contentContainerStyle={{gap:10, padding:10}} columnWrapperStyle={{gap:10}} />
+        
    </View>
   );
 }
