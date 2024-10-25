d59e1bf49b6f67047d8057313362fedb087b6bce
Kodeck James <kodeckjames095@gmail.com>
2024-10-25 16:26:39 +0300

    Eighth commit

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { Stack, useLocalSearchParams } from 'expo-router'
 
 const ProductDetailsScreen = () => {
   const {id} = useLocalSearchParams()
   return (    
     <View className=' bg-white flex-1'>
+      <Stack.Screen options={{title:'Details'}}/>
       <Text >ProductDetailsScreen for id:{id}</Text>
     </View>
   )
--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function MenuStack() {
-    return <Stack/>
+    return <Stack>
+        <Stack.Screen name="index" options={{title:'Menu'}}/>
+    </Stack>
 }
