83e6b8d33d5dbaf8124cfc531f24aca173ac4fa5
Kodeck James <kodeckjames095@gmail.com>
2024-11-03 23:50:11 +0300

    React Context

---
+++ src/providers/CartProvider.tsx
@@ -0,0 +1,11 @@
+// import { Children, createContext } from "react";
+
+// const CartContext = createContext({});
+
+// const CartProvider = ({ Children }) => {
+//     return (
+//         <CartContext.Provider value={{ items: [], onAddItem: () => { } }}>
+//             {Children}
+//         </CartContext.Provider>
+//     )
+// }
